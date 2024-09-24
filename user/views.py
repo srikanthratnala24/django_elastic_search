50038236204ca01f754b78fa0a6e28ed6c7d49e4
srikanthratnala24 <ratnalasrikanth2@gmail.com>
2024-09-24 22:08:24 +0530

    resolved verify email issue

--- user/views.py
+++ user/views.py
@@ -19,19 +19,15 @@ class RegisterView(generics.GenericAPIView):
         user = request.data
         serializer = self.serializer_class(data= user)
         serializer.is_valid(raise_exception=True)
-        # print(serializer.data,'+++++++++++++++++')
 
         serializer.save()
         user_data = serializer.data
         user = CustomUser.objects.get(email=user_data['email'])
 
         token = RefreshToken.for_user(user).access_token
-        current_site = get_current_site(request).domain
+        current_site = request.get_host()
         relative_link = reverse('email-verify')
-        print(current_site,relative_link)
-        port = request.get_port()
-        absurl = 'http://'+ current_site + ":" + port + relative_link + "?token="+str(token)
-        print(absurl) 
+        absurl = 'http://'+ current_site + relative_link + "?token="+str(token) 
 
         email_body = "Hi " + user.username +' Use link below to verify your email \n'+ absurl
         data = {'email_body': email_body,'email_subject':"verify your email",'to_email': user.email}
@@ -43,35 +39,26 @@ class RegisterView(generics.GenericAPIView):
 class VerifyEmail(generics.GenericAPIView):
     serializer_class = VerifyEmailSerializer
 
-    def get(self, request):
+    def get(self,request):
         token = request.GET.get('token')
-        print(token,'token+++++++++++')
-        # payload = jwt.decode(token, settings.SECRET_KEY,algorithms=['HS256'])
-        # print(payload,'++++++++++')
-        try:
-            payload = jwt.decode(token, settings.SECRET_KEY, algorithms= ['HS256'])
 
-            user = User.objects.get(id=payload['user_id'])
-            if not user.is_verified:
+        try:
+
+            payload = jwt.decode(token,settings.SECRET_KEY,algorithms=['HS256'],options={"verify_signature": False})
+            user = CustomUser.objects.get(id=payload['user_id'])
+            if not user.is_verified == True:
                 user.is_verified = True
                 user.save()
-            return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
-        except jwt.ExpiredSignatureError:
-            return Response({'error': 'Activation link has expired'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'email': 'successfully activated' }, status=status.HTTP_200_OK)
 
-        except jwt.exceptions.DecodeError:
-            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        except jwt.exceptions.InvalidSignatureError:
-            return Response({'error': 'Signature verification failed'}, status=status.HTTP_400_BAD_REQUEST)
+    
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error': 'Activation expired' }, status=status.HTTP_400_BAD_REQUEST)
 
-        except User.DoesNotExist:
-            return Response({'error': 'User does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error': 'invalid token' }, status=status.HTTP_400_BAD_REQUEST)
 
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-            
 class LoginAPIView(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
@@ -79,4 +66,3 @@ class LoginAPIView(generics.GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
